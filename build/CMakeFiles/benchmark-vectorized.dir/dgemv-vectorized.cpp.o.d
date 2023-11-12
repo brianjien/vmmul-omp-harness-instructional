8f8e19d3557b1d4044f36e74f8bf3bb3243a5725
brianjien <brianjienop49792399@gmail.com>
2023-11-12 11:08:04 -0800

    ss

--- build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -1,3 +1,21 @@
 CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
  /home/parallels/Desktop/CSC656/vmmul-omp-harness-instructional/dgemv-vectorized.cpp \
- /usr/include/stdc-predef.h
+ /usr/include/stdc-predef.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/arm_neon.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wchar.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/arm_fp16.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/arm_bf16.h
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,19 +8,16 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void dgemv_vectorized(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of vectorized vector-matrix multiply
-   // This is where you will use SIMD instructions or other techniques for vectorization
-   // Example using SIMD intrinsics (change this based on your specific architecture):
-   for (int i = 0; i < n; i++) {
-      __m256d y_vector = _mm256_setzero_pd();
-      for (int j = 0; j < n; j += 4) {
-         __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
-         __m256d x_vector = _mm256_loadu_pd(&x[j]);
-         y_vector = _mm256_add_pd(y_vector, _mm256_mul_pd(a_vector, x_vector));
-      }
-      // Sum the elements of the vector
-      y[i] = y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
-   }
+void my_dgemv(int n, double* A, double* x, double* y) {
+    // Vectorization using AVX (256-bit SIMD)
+    for (int i = 0; i < n; i++) {
+        __m256d y_vector = _mm256_setzero_pd();
+        for (int j = 0; j < n; j += 4) {
+            __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
+            __m256d x_vector = _mm256_loadu_pd(&x[j]);
+            y_vector = _mm256_add_pd(y_vector, _mm256_mul_pd(a_vector, x_vector));
+        }
+        // Sum the elements of the vector
+        y[i] += y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
+    }
 }
-   
