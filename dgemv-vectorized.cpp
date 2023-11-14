76fa3695c7536f799b4dc9627bc1571b4e8f7885
brianjien <brianjienop49792399@gmail.com>
2023-11-14 01:10:29 -0800

    ds

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,4 +1,5 @@
 #include <immintrin.h>
+
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
@@ -9,19 +10,18 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // Assuming A, x, and y are aligned and n is a multiple of 4
+   for(int i = 0; i < n; i++) {
+      __m256d y_vector = _mm256_setzero_pd();
 
+      for (int j = 0; j < n; j += 4) {
+         // Load data with aligned load
+         __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
+         __m256d x_vector = _mm256_loadu_pd(&x[j]);
+         // Perform fused multiply-add
+         y_vector = _mm256_fmadd_pd(a_vector, x_vector, y_vector);
+      }
 
-    __m256d y_vector = _mm256_setzero_pd();
-
-    for (int j = 0; j < n; j += 4) {
-        // Load data with aligned load
-        __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
-        __m256d x_vector = _mm256_loadu_pd(&x[j]);
-        // Perform fused multiply-add
-        y_vector = _mm256_fmadd_pd(a_vector, x_vector, y_vector);
-    }
-
-    // Horizontal sum
-    y[i] += y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
+      // Horizontal sum
+      y[i] += y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
+   }
 }
-
