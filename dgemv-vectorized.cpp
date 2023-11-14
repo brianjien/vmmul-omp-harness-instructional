51219645b83bb25e2be77a5894bc76fc45ecfb6e
brianjien <brianjienop49792399@gmail.com>
2023-11-14 01:35:27 -0800

    cd

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,5 +1,3 @@
-#include <immintrin.h>
-
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
@@ -9,20 +7,11 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-    #pragma omp parallel for
     for (int i = 0; i < n; i++) {
-        __m256d y_vector = _mm256_setzero_pd();
-
-        #pragma omp simd reduction(+:y_vector)
-        for (int j = 0; j < n; j += 4) {
-            // Load data with aligned load
-            __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
-            __m256d x_vector = _mm256_loadu_pd(&x[j]);
-            // Perform fused multiply-add
-            y_vector = _mm256_fmadd_pd(a_vector, x_vector, y_vector);
+        double sum = 0.0;
+        for (int j = 0; j < n; j++) {
+            sum += A[i * n + j] * x[j];
         }
-
-        // Horizontal sum
-        y[i] += y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
+        y[i] += sum;
     }
 }
