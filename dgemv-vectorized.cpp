5903e7ba31be5d2a0d0aa5662e171556b1c7c0f1
brianjien <brianjienop49792399@gmail.com>
2023-11-14 01:02:50 -0800

    ds

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -9,20 +9,18 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void dgemv_vectorized(int n, double* A, double* x, double* y) {
-    // Vectorization using AVX (256-bit SIMD)
-    for (int i = 0; i < n; i++) {
-        __m256d y_vector = _mm256_setzero_pd();
-
-        // Unroll the loop to allow better vectorization
-        for (int j = 0; j < n; j += 4) {
-            // Load data with aligned load
-            __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
-            __m256d x_vector = _mm256_loadu_pd(&x[j]);
-            // Perform fused multiply-add
-            y_vector = _mm256_fmadd_pd(a_vector, x_vector, y_vector);
-        }
-
-        // Horizontal sum
-        y[i] += y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
-    }
+   // insert your code here: implementation of vectorized vector-matrix multiply
+   // This is where you will use SIMD instructions or other techniques for vectorization
+   // Example using SIMD intrinsics (change this based on your specific architecture):
+   for (int i = 0; i < n; i++) {
+      __m256d y_vector = _mm256_setzero_pd();
+      for (int j = 0; j < n; j += 4) {
+         __m256d a_vector = _mm256_loadu_pd(&A[i * n + j]);
+         __m256d x_vector = _mm256_loadu_pd(&x[j]);
+         y_vector = _mm256_add_pd(y_vector, _mm256_mul_pd(a_vector, x_vector));
+      }
+      // Sum the elements of the vector
+      y[i] = y_vector[0] + y_vector[1] + y_vector[2] + y_vector[3];
+   }
 }
+   
